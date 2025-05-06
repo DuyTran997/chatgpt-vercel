2006e37fe177dad68eb13d60e8d796a6e4720edc
DuyTran997 <duytranne258@gmail.com>
2025-05-06 16:05:10 +0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -69,20 +69,20 @@ export default function Header() {
                 </span>
                 <a
                   class="ml-2 <sm:hidden"
-                  href="https://github.com/ourongxing/chatgpt-vercel"
+                  //href="https://github.com/ourongxing/chatgpt-vercel"
                 >
                   <img
                     alt="GitHub forks badge"
-                    src="https://img.shields.io/github/stars/ourongxing/chatgpt-vercel?logo=github"
+                    //src="https://img.shields.io/github/stars/ourongxing/chatgpt-vercel?logo=github"
                   />
                 </a>
                 <a
                   class="ml-2"
-                  href="https://github.com/ourongxing/chatgpt-vercel/fork"
+                  //href="https://github.com/ourongxing/chatgpt-vercel/fork"
                 >
                   <img
                     alt="GitHub forks badge"
-                    src="https://img.shields.io/github/forks/ourongxing/chatgpt-vercel?logo=github"
+                    //src="https://img.shields.io/github/forks/ourongxing/chatgpt-vercel?logo=github"
                   />
                 </a>
               </>
